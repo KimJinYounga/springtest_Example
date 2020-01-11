a3122ac88e44369d59492b8e9828660d2a036f3b
DESKTOP-김진영\rlawl <rlawlsdud419@naver.com>
2020-01-11 20:16:16 +0900

    Initial commit

---
+++ src/test/java/me/jinyoung/springdemo/sample/SampleControllerTest.java
@@ -0,0 +1,58 @@
+package me.jinyoung.springdemo.sample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.MOCK)
+//@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
+//@AutoConfigureMockMvc
+@WebMvcTest(SampleController.class)
+public class SampleControllerTest {
+    /* Mock버전
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello jinyoung"))
+                .andDo(print());
+    }
+     */
+
+//    @Autowired
+//    TestRestTemplate testRestTemplate;
+
+
+    @MockBean
+    SampleService mockSampleService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        when(mockSampleService.getName()).thenReturn("whiteship");
+        mockMvc.perform(get("/hello"))
+                .andExpect(content().string("hello whiteship"));
+//        String result=testRestTemplate.getForObject("/hello", String.class);
+//        assertThat(result).isEqualTo("hello whiteship");
+    }
+}
